e3b14d52af1241998359c95570fd7df03267ebd6
dmz4484 <108957656+dmz4484@users.noreply.github.com>
2025-09-25 00:31:15 -0500

    Update Skills.js

--- src/containers/skills/Skills.js
+++ src/containers/skills/Skills.js
@@ -21,8 +21,8 @@ export default function Skills() {
               <DisplayLottie animationData={codingPerson} />
             ) : (
               <img
-                alt="Man Working"
-                src={require("../../assets/images/developerActivity.svg")}
+                alt="skill"
+                src={require("../../assets/images/skills_img.jpg")}
               ></img>
             )}
           </div>
